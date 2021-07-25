e952ab04d625a29d15a8a75d377364b4512ac171
Ricardo <rr155914@gmail.com>
2021-07-25 18:57:51 -0500

    Add door

--- src/game.ts
+++ src/game.ts
@@ -5,4 +5,8 @@ const house = new Entity();
 engine.addEntity(house);
 house.addComponent(new GLTFShape("models/floor.glb"));
 house.addComponent(new Transform({ position: new Vector3(10,0,20)}));
- 
+
+const door = new Entity();
+engine.addEntity(door);
+door.addComponent(new GLTFShape("models/door.glb"));
+door.addComponent(new Transform({ position: new Vector3(10,0,20)}));
