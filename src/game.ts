f0474cfb443a0d8b1c6d4bec3bb46f384af75d15
Ricardo <rr155914@gmail.com>
2021-07-25 19:15:51 -0500

    Add functions to door

--- src/game.ts
+++ src/game.ts
@@ -10,3 +10,14 @@ const door = new Entity();
 engine.addEntity(door);
 door.addComponent(new GLTFShape("models/door.glb"));
 door.addComponent(new Transform({ position: new Vector3(10,0,20)}));
+
+
+//Define two different materials
+let startPosition = new Vector3(10,0,20);
+let finalPosition = new Vector3(20,0,20);
+
+door.addComponent(
+    new OnClick(event => {
+        door.addComponent(new utils.MoveTransformComponent(startPosition, finalPosition, 2))
+    })
+  );
