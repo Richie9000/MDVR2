131922decd3647723c9e4bbbeb0feb44f06f4ce8
Ricardo <rr155914@gmail.com>
2021-07-26 13:06:29 -0500

    Check openclose door function

--- src/game.ts
+++ src/game.ts
@@ -2,22 +2,40 @@ import * as utils from '@dcl/ecs-scene-utils'
 
 
 const house = new Entity();
-engine.addEntity(house);
 house.addComponent(new GLTFShape("models/floor.glb"));
 house.addComponent(new Transform({ position: new Vector3(10,0,20)}));
+engine.addEntity(house);
 
 const door = new Entity();
-engine.addEntity(door);
 door.addComponent(new GLTFShape("models/door.glb"));
 door.addComponent(new Transform({ position: new Vector3(10,0,20)}));
+engine.addEntity(door);
+
+const button = new Entity();
+button.addComponent(new GLTFShape("models/doorbtn.glb"));
+button.addComponent(new Transform({ position: new Vector3(10,0,20)}));
+engine.addEntity(button);
 
 
-//Define two different materials
 let startPosition = new Vector3(10,0,20);
 let finalPosition = new Vector3(10,4,20);
 
-door.addComponent(
-    new OnClick(event => {
-        door.addComponent(new utils.MoveTransformComponent(startPosition, finalPosition, 2))
+button.addComponent(
+    new utils.ToggleComponent(utils.ToggleState.Off, value => {
+        if (value == utils.ToggleState.On) {
+            door.addComponentOrReplace(
+                new utils.MoveTransformComponent(startPosition, finalPosition, 2)) 
+      } else {
+        door.addComponentOrReplace(new utils.MoveTransformComponent(finalPosition, startPosition, 2)) 
+        
+      }
     })
-  );
+  )
+  
+  //listen for click on the box and toggle it's state
+  button.addComponent(
+    new OnClick(event => {
+      button.getComponent(utils.ToggleComponent).toggle()
+    })
+  )
+  
