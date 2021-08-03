7f0336df6feb59bbc1dc656de01e296e3fee8e29
Ricardo <rr155914@gmail.com>
2021-08-03 16:35:26 -0500

    Add more buttons

--- src/game.ts
+++ src/game.ts
@@ -13,19 +13,29 @@ door.addComponent(new Transform({ position: new Vector3(10,0,20)}));
 engine.addEntity(door);
 
 const button = new Entity();
-button.addComponent(new GLTFShape("models/button1.glb"));
+button.addComponent(new GLTFShape("models/button.glb"));
 button.addComponent(new Transform({ position: new Vector3(10,0,20)}));
 engine.addEntity(button);
 
 const button1 = new Entity();
-button1.addComponent(new GLTFShape("models/button4.glb"));
+button1.addComponent(new GLTFShape("models/button1.glb"));
 button1.addComponent(new Transform({ position: new Vector3(10,0,20)}));
 engine.addEntity(button1);
 
 const button2 = new Entity();
-button2.addComponent(new GLTFShape("models/button3.glb"));
+button2.addComponent(new GLTFShape("models/button2.glb"));
 button2.addComponent(new Transform({ position: new Vector3(10,0,20)}));
 engine.addEntity(button2);
+
+const button3 = new Entity();
+button3.addComponent(new GLTFShape("models/btncube2.glb"));
+button3.addComponent(new Transform({ position: new Vector3(10,0,20)}));
+engine.addEntity(button3);
+
+const button4 = new Entity();
+button4.addComponent(new GLTFShape("models/btncube3.glb"));
+button4.addComponent(new Transform({ position: new Vector3(10,0,20)}));
+engine.addEntity(button4);
 
 let startPosition = new Vector3(10,0,20);
 let finalPosition = new Vector3(7,0,20);
@@ -66,8 +76,26 @@ button1.addComponent(
 button2.addComponent(
   new OnPointerDown(
     (e) => {
-      movePlayerTo({ x:8, y: 15, z: 19 }, { x: 8, y: 1, z: 8 })
+      movePlayerTo({ x:8, y: 19, z: 19 }, { x: 8, y: 1, z: 8 })
     },
     { hoverText: "Room 2" }
+  )
+)
+
+button3.addComponent(
+  new OnPointerDown(
+    (e) => {
+      movePlayerTo({ x:5, y: 3, z: 20 }, { x: 10, y: 0, z: 20 })
+    },
+    { hoverText: "Living Room" }
+  )
+)
+
+button4.addComponent(
+  new OnPointerDown(
+    (e) => {
+      movePlayerTo({ x:5, y: 3, z: 20}, { x: 10, y: 0, z: 20 })
+    },
+    { hoverText: "Living Room" }
   )
 )
