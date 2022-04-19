6234dc91ab88c6917475ebb93d93e8777f0a467a
Ricardo <rr155914@gmail.com>
2022-04-19 09:10:45 -0500

    Testing materials

--- src/game.ts
+++ src/game.ts
@@ -2,11 +2,5 @@ import { displayEvent, createEventsBoard } from './eventBoard'
 import { House } from './house'
 
 
-createEventsBoard({
-  position: new Vector3(25,17.5,20),
-  rotation: Quaternion.Euler(0, 130, 0),
-  scale: new Vector3(.6, .6, .8),
-});
-
 House();
 
